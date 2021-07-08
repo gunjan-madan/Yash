5ca7e49b8cc39b4c60a9445db6cd98c6f7fe047e
gunjan-madan <53206795+gunjan-madan@users.noreply.github.com>
2021-07-08 16:46:36 +0530

    add

--- Module2/3.1.py
+++ Module2/3.1.py
@@ -1,2 +1,29 @@
 # In the previous lesson, you used if-elif-else statements to create a menu based program.
 # Now let us take a look at using nested if-elif statements in creating menu based programs.
+'''Task 1: Nested If-else'''
+print()
+print("*** Task 1: ***")
+print()
+#Make a variable like winning_number and assign any number to it between 1 and 20.
+#Ask user to guess a number between 1 and 20. (Take input)
+#if user guessed correctly, print "YOU WIN"
+#if user didn't guessed correctly then:
+  #if user guessed lower than actual number, then print "TOO LOW"
+   #if user guessed lower than actual number, then print "TOO HIGH"
+
+'''Task 2: Nested If-else'''
+print()
+print("*** Task 2: ***")
+print()
+#This is a program to tell User the shipping cost based on the country and the weight.
+#Write a Program that takes two inputs: country_code(AU/US) and weight of the product.
+#Use the following conditions to find the shipping cost
+#country          Product Size               Shippping cost
+#US               less than 1kg               $5
+#US               between 1 and 2kg           $10
+#US               greater than 2kg            $20
+#AU               less than 1kg               $10
+#AU               between 1 and 2kg           $15
+#AU               greater than 2kg            $25
+
+# print("This Program will caluculate Shipping Cost")
---
+++ Module2/3.HW.py
@@ -0,0 +1,11 @@
+#Practice on Nested Else if
+
+''' Task 1: Metric Convertor'''
+print("**** Task 1:  ****")
+print()
+# Write a Metric Conversion program that converts from one metric to another metric.
+# You can start with:
+# 1. cm to inch (Hint: divide the cm value by 2.54)
+# 2. inch to feet (Hint: divide the inch value by 12)
+# 3. meter to kilometer (Hint: divide the meter value by 1000)
+# You can add more to the list.
